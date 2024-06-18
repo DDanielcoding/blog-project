94c402db68f56a41ee429da69a9173f18745f6eb
DDanielcoding <draskoczi.daniel@gmail.com>
2024-06-18 18:32:19 +0200

    Logs are now stored individually.

--- models/comment.go
+++ models/comment.go
@@ -4,6 +4,7 @@ import "time"
 
 type Comment struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
+	Username  string    `json:"username"`
 	BlogID    uint      `json:"blog_id"`
 	AuthorID  uint      `json:"author_id"`
 	Content   string    `json:"content"`
